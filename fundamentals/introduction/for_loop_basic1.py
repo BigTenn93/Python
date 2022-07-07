c85ee3244bc37a115205fecc18b8ecc93112e396
Taylor Tenner <mylait93@gmail.com>
2022-07-07 10:02:42 -0500

    forloopbasic1, algos7_6

---
+++ fundamentals/introduction/for_loop_basic1.py
@@ -0,0 +1,39 @@
+# for i in range(151):
+#     print(i)
+
+# for i in range(5, 1005, 5):
+#     print(i)
+
+# def dojo_way():
+#     for i in range (1, 101, 1):
+#         print (i)
+#     if i % 5 == 0:
+#         print ('Coding')
+#     if i % 10 == 0:
+#         print ('Coding Dojo')
+
+# minimum = 0
+# maximum = 500000
+# odds = 0
+
+# for num in range(minimum, maximum+1):
+#     if(num % 2 != 0):
+#         print("{0}".format(num))
+#         odds = odds + num
+
+# print("The sum of odd numbers from {0} to {1} = {2}".format(minimum, maximum, odds))
+
+# def count_down():
+#     num = 2018
+#     while num > 0:
+#         print(num)
+#         num = num - 4
+
+# count_down()
+
+# def flex_count(low, high, multi):
+#     for i in range (low, high):
+#         if i % multi == 0:
+#             print(i)
+
+# flex_count(2, 9, 3)
