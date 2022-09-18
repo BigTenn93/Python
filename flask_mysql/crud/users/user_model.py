f615c5aafe6b06413be2e11ae97328e470199224
Taylor Tenner <mylait93@gmail.com>
2022-09-18 13:39:02 -0500

    users unfinished

---
+++ flask_mysql/crud/users/user_model.py
@@ -0,0 +1,20 @@
+from mysqlconnection import connectToMySQL
+DATABASE = 'users_db'
+
+class User:
+    def __init__( self , data ):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('first_flask').query_db(query)
+        users = []
+        for user in results:
+            users.append( cls(user) )
+        return users
